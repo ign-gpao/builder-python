7594e362a78a9a17e447e18f09159bec20cdb991
Arnaud Birk <arnaud.birk@ign.fr>
2023-08-28 10:43:59 +0200

    fix dependencies

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 """ script test """
+import json
 
 from gpao.builder import Builder
 from gpao.project import Project
 from gpao.job import Job
+
+
+def is_valid(json1, json2):
+    """ Fonction test """
+
+    with open(json1, encoding="utf-8") as file:
+        data1 = json.load(file)
+
+    with open(json2, encoding="utf-8") as file:
+        data2 = json.load(file)
+
+    if data1 == data2:
+        return True
+
+    return False
+
 
 job1 = Job("job1", "touch file", tags=["tag1", "tag2"])
 job2 = Job("job2", "touch file")
@@ -34,6 +51,13 @@ builder = Builder([project1, project2])
 builder.add_project(project3)
 
 builder.save_as_json("project.json")
+builder.save_as_json("project_bis.json")
+
+if is_valid("project.json", "project_bis.json"):
+    print("sending to API")
+    builder.send_project_to_api("http://localhost:8080")
+else:
+    print("Error, not same json")
 
 
 job8 = Job("job8", "touch file")
@@ -45,5 +69,11 @@ project5 = Project("project5",  [job9], [project4])
 
 builder = Builder([project4, project5])
 
-# builder.save_as_json("project2.json")
-builder.send_project_to_api("http://localhost:8080")
+builder.save_as_json("project2.json")
+builder.save_as_json("project2_bis.json")
+
+if is_valid("project2.json", "project2_bis.json"):
+    print("sending to API")
+    builder.send_project_to_api("http://localhost:8080")
+else:
+    print("Error, not same json")
